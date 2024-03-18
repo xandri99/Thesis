eeeba86a98d4a89444fc3d3bb953936d08969cf3
Bernat Xandri Zaragoza <bxz1999@gmail.com>
2024-03-18 16:21:27 +0100

    sss

--- src/WGCNA_functions.py
+++ src/WGCNA_functions.py
@@ -1,38 +1,74 @@
 """
-WGCNA Implementation Using Numpy. All methods are commented callable functions, for an easier 
-implementation.
+WGCNA Implementation Using Numpy. 
+All methods are commented callable functions, for an easier use.
 
 """
 
+################################################################################################
+#                                        IMPORTS
+################################################################################################
 
-def preprocessing(number: float) -> float:
-    """
-    Step 1: Data Preprocessing (Normalization)
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import os
+import sys
+import gc
 
-    Preprocessing: removing obvious outlier on genes and samples
-    
-    """
-    print(f"{BOLD}{OKBLUE}Pre-processing...{ENDC}")
+from statsmodels.formula.api import ols
+
+import scipy.cluster.hierarchy as scipy_hierarchy
+from scipy.spatial.distance import squareform
+from scipy import stats
+
+from sklearn.decomposition import PCA
+
+################################################################################################
 
 
 
-    ## Prepare and clean data
-    # Remove genes expressed under this cutoff number along samples - use clustering of samples
+
+
+################################################################################################
+#                                       CONFIGURATIONS
+################################################################################################
+
+# Settings for printing dataframes
+pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 20)
+pd.set_option('display.width', 1000)
+
+# Colors for the terminal outputs
+ENDC = "\033[0m"
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+
+OKBLUE = "\033[94m"
+OKGREEN = "\033[92m"
+WARNING = "\033[93m"
+FAIL = "\033[91m"
+
+################################################################################################
 
 
 
-    ## Remove genes with no variation across samples (0 vectors) 
-    transcriptomics_dataset_filtered = transcriptomics_dataset.loc[:, (transcriptomics_dataset != 0).any(axis=0)] # Actually droping columns with 0 variation
+
+
+################################################################################################
+#                                       PREPROCESSING
+################################################################################################
+
+def simple_preprocessing():
+    # Step 1: Filter out genes with low expression across all samples
+    # Threshold set so that a gene needs to be expressed TPM > 1 in at least 10% of samples## Remove genes with no variation across samples (0 vectors) 
+    cleaned_dataset = raw_data.loc[:, (raw_data != 0).any(axis=0)] # Actually droping columns with 0 variation
 
     # Also print how many genes have been removed in this steo
-    num_genes_removed = transcriptomics_dataset.shape[1] - transcriptomics_dataset_filtered.shape[1]
+    num_genes_removed = raw_data.shape[1] - cleaned_dataset.shape[1]
     print(f"{BOLD}{WARNING}{num_genes_removed} genes were removed due to having 0 variation across samples...{ENDC}")
+    
+    return cleaned_dataset
 
 
 
-    # NOTES: Maybe no onlyt genes with no variation, but small variation.
-
-
-
-
-    print(f"{BOLD}{OKBLUE}Done...{ENDC}")
